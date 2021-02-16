0a54dce26f2324e26dfee9e189932cd73fcf5c28
amir415353 <75042630+amir415353@users.noreply.github.com>
2021-02-16 20:51:49 +0330

    Update gtor.py

--- gtor.py
+++ gtor.py
@@ -1,9 +1,9 @@
 import time
 import os
 import subprocess
-import sys
+
 for a in range(0, 9999):
     os.system('pkill tor')
-    subprocess.Popen(["tor"])
+    proc = subprocess.Popen(["tor"])
     time.sleep(12)
-    sys.exit(0)
+    proc.terminate()
