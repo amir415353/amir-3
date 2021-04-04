1f72bd6d84ad9fc37439c8e7baa8e1d766ee0850
amir415353 <75042630+amir415353@users.noreply.github.com>
2021-04-04 23:00:49 +0430

    Update icut.py

--- icut.py
+++ icut.py
@@ -1,23 +1,29 @@
-import pyautogui
 import cv2
+import time
+from mss import mss
 try:
     from PIL import Image
 except ImportError:
     import Image
 import pytesseract
 
+time.sleep(5)
+with mss() as sct:
+    sct.shot()
+
+exit()
 pytesseract.pytesseract.tesseract_cmd = r'F:\Program Files\ocr\tesseract.exe'
-img = cv2.imread("index.png")
-y = 264
-x = 84
-h = 483
-w = 482
+img = cv2.imread("monitor-1.png")
+y = 262
+x = 93
+h = 385
+w = 384
 crop_img = img[y:y+h, x:x+w]
 cv2.imwrite(r'sc2.jpg', crop_img)
-y = 167
-x = 84
-h = 42
-w = 482
+y = 182
+x = 96
+h = 29
+w = 200
 crop_img = img[y:y+h, x:x+w]
 cv2.imwrite(r'index2.png', crop_img)
 a = pytesseract.image_to_string(Image.open('index2.png'))
@@ -26,5 +32,3 @@ print(b)
 print(b[0])
 exit()
 
-myScreenshot = pyautogui.screenshot()
-myScreenshot.save(r'sc.jpg')
